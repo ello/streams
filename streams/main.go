9082e49c285c76528a58634ea6cbdb3675644297
Ryan Tyer <ryan.tyer@gmail.com>
2015-11-18 23:29:49 -0700

    Added verbose logging options to the API

--- streams/main.go
+++ streams/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/negroni"
 	"github.com/ello/ello-go/common/util"
 	"github.com/ello/ello-go/streams/api"
 	"github.com/ello/ello-go/streams/service"
 	"github.com/julienschmidt/httprouter"
+	nlog "github.com/meatballhat/negroni-logrus"
 )
 
+var verbose bool
+var veryVerbose bool
+
 func main() {
-	log.SetLevel(log.DebugLevel)
+
+	flag.BoolVar(&verbose, "v", false, "if set, show messages from the logger")
+	flag.BoolVar(&veryVerbose, "vv", false, "if set, show ALL messages from the logger")
+
+	flag.Parse()
+
+	logLevel := log.WarnLevel
+	if verbose {
+		logLevel = log.InfoLevel
+	}
+	if veryVerbose {
+		logLevel = log.DebugLevel
+	}
+	log.SetLevel(logLevel)
 
 	streamsService, err := service.NewRoshiStreamService("http://localhost:6302")
 	if err != nil {
@@ -30,8 +49,13 @@ func main() {
 	// controllers register their routes with the router
 	streamsController.Register(router)
 
+	n := negroni.New(
+		negroni.NewRecovery(),
+		nlog.NewCustomMiddleware(logLevel, &log.TextFormatter{}, "web"),
+	)
+	n.UseHandler(router)
+
 	port := util.GetEnvWithDefault("ELLO_API_PORT", "8080")
-	serverAt := "localhost:" + port
-	log.Debugf("Listening at: %v", serverAt)
-	http.ListenAndServe(serverAt, router)
+	serverAt := ":" + port
+	n.Run(serverAt)
 }
