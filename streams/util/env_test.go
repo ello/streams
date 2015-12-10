08abe1b224583e1ff2a9c342308845e1db2c89bb
Ryan Tyer <ryan.tyer@gmail.com>
2015-12-09 21:13:26 -0700

    Cleaning up code, adding tests

---
+++ streams/util/env_test.go
@@ -0,0 +1,50 @@
+package util_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/ello/ello-go/streams/util"
+)
+
+func ExampleGetEnvWithDefault() {
+	//Key doesn't exist
+	myval := util.GetEnvWithDefault("mykey", "This is the default")
+
+	fmt.Printf("%v", myval) // Output: This is the default
+}
+
+func TestGetEnvWithDefault(t *testing.T) {
+	key := "AbC_123"
+	key2 := "AAAZZZ___"
+	val := "zzddzz023adfg12345"
+	_ = os.Setenv(key, val)
+
+	result := util.GetEnvWithDefault(key, "default")
+	if result != val {
+		t.Error("Default value should not be returned")
+	}
+
+	result = util.GetEnvWithDefault(key2, "default")
+	if result != "default" {
+		t.Error("Default value should not be returned")
+	}
+}
+
+func TestIsEnvPresent(t *testing.T) {
+	key := "AbC_123"
+	key2 := "AAAZZZ___"
+	val := "zzddzz023adfg12345"
+	_ = os.Setenv(key, val)
+
+	result := util.IsEnvPresent(key)
+	if !result {
+		t.Error("Key is present, result should be true")
+	}
+
+	result = util.IsEnvPresent(key2)
+	if result {
+		t.Error("Key is not present, result should be false")
+	}
+}
